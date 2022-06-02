79e75e326a658f6b40f9ea8129ef8df510009813
swissbuechi <raphael.buechi@hotmail.com>
2022-06-02 13:59:50 +0200

    Added Exo Report

---
+++ AzureAdDeployer/AzureAdDeployer.ps1
@@ -0,0 +1,172 @@
+#CSS codes
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $false)]
+    [switch]$Install,
+    [Parameter(Mandatory = $false)]
+    [switch]$SetDefault,    
+    [Parameter(Mandatory = $false)]
+    [switch]$UseExistingExoSession
+    # [Parameter(Mandatory = $false)]
+    # [string[]]$Paths = $null
+)
+$Version = "1.0.0"
+$script:ExoConnected = $false
+
+Write-Host "AzureAdDeployer Version " $Version
+
+$ReportTitle = "AzureAdDeployer Report"
+$ReportTitleHtml = "<h1>" + $ReportTitle + "</h1>"
+
+$PostContentHtml = "<p id='CreationDate'>Creation Date: $(Get-Date)</p>"
+
+function installEXO {
+    if (Get-Module -Name ExchangeOnlineManagement -ListAvailable) {
+        Write-Host "Updating PowerShell ExchangeOnline Module"
+        Update-Module -Name ExchangeOnlineManagement -Scope CurrentUser -Force
+    }
+
+    if (-not (Get-Module -Name ExchangeOnlineManagement -ListAvailable)) {
+        Write-Host "Installing PowerShell ExchangeOnline Module"
+        Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser -Force
+    }
+
+}
+
+function connectExo {
+    if ($UseExistingExoSession) { return }
+    if (-not $script:ExoConnected) {
+        Write-Host "Connecting to EXO"
+        Connect-ExchangeOnline -ShowBanner:$false
+    }
+    $script:ExoConnected = $true
+}
+
+function disconnectExo {
+    if ($UseExistingExoSession) { return }
+    if ($script:ExoConnected) {
+        Write-Host "Disconnecting to EXO"
+        Disconnect-ExchangeOnline -Confirm:$false
+    }
+    $script:ExoConnected = $false
+}
+
+function checkMailboxLang {
+    connectExo
+    Write-Host "Generating User Mailbox Language Report"
+    return Get-EXOMailbox -ResultSize:Unlimited | Get-MailboxRegionalConfiguration | ConvertTo-HTML -As Table -Property Identity, Language, TimeZone `
+        -Fragment -PreContent "<h2>User Mailbox Language</h2>"
+}
+
+function setMailboxLang {
+    connectExo
+    Write-Host "Set Mailbox Lang CH"
+    Get-EXOMailbox | Set-MailboxRegionalConfiguration -LocalizeDefaultFolderName:$true -Language de-CH -TimeZone "W. Europe Standard Time" 
+}
+
+function checkSharedMailbox {
+    connectExo
+    Write-Host "Checking SharedMailboxes"
+    return Get-EXOMailbox -RecipientTypeDetails SharedMailbox -ResultSize:Unlimited -Properties DisplayName, UserPrincipalName, `
+        MessageCopyForSentAsEnabled, MessageCopyForSendOnBehalfEnabled | ConvertTo-HTML -As Table -Property DisplayName, `
+        UserPrincipalName, MessageCopyForSentAsEnabled, MessageCopyForSendOnBehalfEnabled -Fragment -PreContent "<h2>Shared Mailbox Overview</h2>"
+}
+
+function setSharedMailboxEnableCopyToSent {
+    connectExo
+    Write-Host "SharedMailbox enable copy to sent folder"
+    Get-EXOMailbox -RecipientTypeDetails SharedMailbox -ResultSize:Unlimited | Set-Mailbox -MessageCopyForSentAsEnabled $True -MessageCopyForSendOnBehalfEnabled $True
+}
+
+if ($Install) {
+    Write-Host "Installing prerequisite"
+    installEXO
+    return
+}
+
+if ($SetDefault) {
+    Write-Host "Set Default Settings"
+    setMailboxLang
+    setSharedMailboxEnableCopyToSent
+}
+
+$MailboxLang = checkMailboxLang
+$SharedMailbox = checkSharedMailbox
+
+disconnectExo
+
+
+$Header = @"
+<style>
+
+h1 {
+
+    font-family: Arial, Helvetica, sans-serif;
+    color: #666666;
+    font-size: 28px;
+
+}
+
+
+h2 {
+
+    font-family: Arial, Helvetica, sans-serif;
+    color: #666666;
+    font-size: 16px;
+    
+}
+
+
+table {
+    font-size: 12px;
+    border: 0px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+td {
+    padding: 4px;
+    margin: 0px;
+    border: 0;
+}
+
+th {
+    background: #666666;
+    color: #fff;
+    font-size: 11px;
+    text-transform: uppercase;
+    padding: 10px 15px;
+    vertical-align: middle;
+}
+
+tbody tr:nth-child(even) {
+    background: #f0f0f2;
+}
+
+
+#CreationDate {
+
+    font-family: Arial, Helvetica, sans-serif;
+    color: #666666;
+    font-size: 12px;
+
+}
+
+
+.StopStatus {
+
+    color: #ff0000;
+}
+
+
+.RunningStatus {
+    
+    color: #008000;
+}
+
+</style>
+"@
+
+Write-Host "Generating HTML Report"
+$Report = ConvertTo-HTML -Body "$ReportTitleHtml $MailboxLang $SharedMailbox" -Title $ReportTitle -Head $Header -PostContent $PostContentHtml
+$Report | Out-File .\AzureAdDeployer-Report.html
+Invoke-Item .\AzureAdDeployer-Report.html
--- AzureAdDeployer/samples/report-Sample-3.ps1
+++ AzureAdDeployer/samples/report-Sample-3.ps1
@@ -77,28 +77,31 @@ $header = @"
 "@
 
 #The command below will get the name of the computer
-$ComputerName = "<h1>Computer name: $env:computername</h1>"
+$ComputerName = "<h1>OS name: " + $PSVersionTable.OS + "</h1>"
+
+$Packages = Get-Package | ConvertTo-Html -As List -Property Name, Version -Fragment -PreContent "<h2>Package Information</h2>"
 
 #The command below will get the Operating System information, convert the result to HTML code as table and store it to a variable
-$OSinfo = Get-CimInstance -Class Win32_OperatingSystem | ConvertTo-Html -As List -Property Version,Caption,BuildNumber,Manufacturer -Fragment -PreContent "<h2>Operating System Information</h2>"
+# $OSinfo = Get-CimInstance -Class Win32_OperatingSystem | ConvertTo-Html -As List -Property Version,Caption,BuildNumber,Manufacturer -Fragment -PreContent "<h2>Operating System Information</h2>"
 
 #The command below will get the Processor information, convert the result to HTML code as table and store it to a variable
-$ProcessInfo = Get-CimInstance -ClassName Win32_Processor | ConvertTo-Html -As List -Property DeviceID,Name,Caption,MaxClockSpeed,SocketDesignation,Manufacturer -Fragment -PreContent "<h2>Processor Information</h2>"
+# $ProcessInfo = Get-CimInstance -ClassName Win32_Processor | ConvertTo-Html -As List -Property DeviceID,Name,Caption,MaxClockSpeed,SocketDesignation,Manufacturer -Fragment -PreContent "<h2>Processor Information</h2>"
 
 #The command below will get the BIOS information, convert the result to HTML code as table and store it to a variable
-$BiosInfo = Get-CimInstance -ClassName Win32_BIOS | ConvertTo-Html -As List -Property SMBIOSBIOSVersion,Manufacturer,Name,SerialNumber -Fragment -PreContent "<h2>BIOS Information</h2>"
+# $BiosInfo = Get-CimInstance -ClassName Win32_BIOS | ConvertTo-Html -As List -Property SMBIOSBIOSVersion,Manufacturer,Name,SerialNumber -Fragment -PreContent "<h2>BIOS Information</h2>"
 
 #The command below will get the details of Disk, convert the result to HTML code as table and store it to a variable
-$DiscInfo = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | ConvertTo-Html -As List -Property DeviceID,DriveType,ProviderName,VolumeName,Size,FreeSpace -Fragment -PreContent "<h2>Disk Information</h2>"
+# $DiscInfo = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | ConvertTo-Html -As List -Property DeviceID,DriveType,ProviderName,VolumeName,Size,FreeSpace -Fragment -PreContent "<h2>Disk Information</h2>"
 
 #The command below will get first 10 services information, convert the result to HTML code as table and store it to a variable
-$ServicesInfo = Get-CimInstance -ClassName Win32_Service | Select-Object -First 10  |ConvertTo-Html -Property Name,DisplayName,State -Fragment -PreContent "<h2>Services Information</h2>"
-$ServicesInfo = $ServicesInfo -replace '<td>Running</td>','<td class="RunningStatus">Running</td>'
-$ServicesInfo = $ServicesInfo -replace '<td>Stopped</td>','<td class="StopStatus">Stopped</td>'
+# $ServicesInfo = Get-CimInstance -ClassName Win32_Service | Select-Object -First 10  |ConvertTo-Html -Property Name,DisplayName,State -Fragment -PreContent "<h2>Services Information</h2>"
+# $ServicesInfo = $ServicesInfo -replace '<td>Running</td>','<td class="RunningStatus">Running</td>'
+# $ServicesInfo = $ServicesInfo -replace '<td>Stopped</td>','<td class="StopStatus">Stopped</td>'
 
   
 #The command below will combine all the information gathered into a single HTML report
-$Report = ConvertTo-HTML -Body "$ComputerName $OSinfo $ProcessInfo $BiosInfo $DiscInfo $ServicesInfo" -Head $header -Title "Computer Information Report" -PostContent "<p id='CreationDate'>Creation Date: $(Get-Date)</p>"
+# $Report = ConvertTo-HTML -Body "$ComputerName $OSinfo $ProcessInfo $BiosInfo $DiscInfo $ServicesInfo" -Head $header -Title "Computer Information Report" -PostContent "<p id='CreationDate'>Creation Date: $(Get-Date)</p>"
+$Report = ConvertTo-HTML -Body "$ComputerName $Packages" -Head $header -Title "Information Report" -PostContent "<p id='CreationDate'>Creation Date: $(Get-Date)</p>"
 
 #The command below will generate the report to an HTML file
 $Report | Out-File .\Basic-Computer-Information-Report.html
