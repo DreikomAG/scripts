02c699004b99cc373d977ea116860f80c82fe8a0
Raphael Büchi <raphael.buechi@hotmail.com>
2022-12-19 23:01:50 +0100

    Update FsLogixExcludeUser.ps1

--- Azure/AVD/FsLogixExcludeUser.ps1
+++ Azure/AVD/FsLogixExcludeUser.ps1
@@ -1,3 +1,3 @@
-$users = "AzureAD\admin@dreikom.onmicrosoft.com", "AzAdministrator"
+$users = "AzureAD\admin@<domain>.onmicrosoft.com", "AzAdministrator"
 $group = "FSLogix Profile Exclude List"
 Add-LocalGroupMember -Group $group -Member $users -ErrorAction SilentlyContinue
