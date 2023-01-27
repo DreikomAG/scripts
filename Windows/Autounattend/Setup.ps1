69059d1a36f226b0fbdd11fd3aa9cdc327d08cfc
swissbuechi <raphael.buechi@hotmail.com>
2023-01-27 14:18:39 +0100

    fix path

--- Windows/Autounattend/Setup.ps1
+++ Windows/Autounattend/Setup.ps1
@@ -2,6 +2,6 @@
 REG add "HKLM\Software\MyIntuneApps" /v "SetLanguage-de-CH" /t REG_DWORD /d 1 /f /reg:64 | Out-Null
 netsh wlan add profile filename=D:\WlanTecTest.xml user=all
 
-.\TestConnection.ps1 -Script .\Chocolatey.ps1
+D:\TestConnection.ps1 -Script D:\Chocolatey.ps1
 
-if (.\CheckHP.ps1) { .\TestConnection.ps1 -Script .\Hpia.ps1 }
+if (D:\CheckHP.ps1) { D:\TestConnection.ps1 -Script D:\Hpia.ps1 }
