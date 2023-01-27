1587b1108f305030d9aad3bca555a0cc3f81a348
swissbuechi <raphael.buechi@hotmail.com>
2023-01-27 15:45:10 +0100

    fix wait for process

--- Windows/Autounattend/Hpia.ps1
+++ Windows/Autounattend/Hpia.ps1
@@ -4,6 +4,6 @@ $ExePath = "C:\Windows\Temp\$Exe"
 
 Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/hpia/$Exe" -OutFile $ExePath
 
-Invoke-Expression "$ExePath /s /e /f C:\Hpia"
+Start-Process -wait "$ExePath /s /e /f C:\Hpia"
 
-Invoke-Expression "C:\Hpia\HPImageAssistant.exe /Operation:Analyze /Category:All /Selection:All /Action:Install /Silent /ReportFolder:C:\Hpia\HPIAReport /Softpaqdownloadfolder:C:\Hpia\HPIASoftpaqs"
+Start-Process -wait "C:\Hpia\HPImageAssistant.exe /Operation:Analyze /Category:All /Selection:All /Action:Install /Silent /ReportFolder:C:\Hpia\HPIAReport /Softpaqdownloadfolder:C:\Hpia\HPIASoftpaqs"
