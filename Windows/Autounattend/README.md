7b94d366701a7809511ede4dcc9d640a1dd7d4b1
Raphael Büchi <raphael.buechi@dreikom.ch>
2023-01-31 14:51:40 +0100

    fix wlan profile

---
+++ Windows/Autounattend/README.md
@@ -0,0 +1,21 @@
+# Autounattend
+
+## Logs
+
+Setup.ps1: `C:\Temp\Autounattend.log`
+
+Chocolatey: `C:\ProgramData\chocolatey\logs\chocolatey.log`
+
+## Tasks
+
+- Set Registry key for de-CH Language Pack
+
+- Import WLAN profile from wifi.txt
+
+- Install Chocolatey
+
+- Install Adobe Reader: `choco install adobereader -y`
+
+- Install HP Image Assistant
+
+- Run HP Image Assistant Driver install
--- Windows/Autounattend/Setup.ps1
+++ Windows/Autounattend/Setup.ps1
@@ -1,10 +1,14 @@
-﻿Start-Transcript -Path "C:\Temp\Autounattend.log"
+﻿$Version = "1.0.0"
+Start-Transcript -Path "C:\Temp\Autounattend.log"
+Write-Host "Version: $Version"
+Set-Location D:\
 Set-ExecutionPolicy Unrestricted -Scope Process
 REG add "HKLM\Software\MyIntuneApps" /v "SetLanguage-de-CH" /t REG_DWORD /d 1 /f /reg:64 | Out-Null
-netsh wlan add profile filename=D:\WlanTecTest.xml user=all
-D:\TestConnection.ps1 -Script "Start-Sleep -Seconds 30" # Wait for WLAN
+$WlanConfig = ".\wifi.txt"
+.\SetupWifi.ps1 -SSID (Get-Content $WlanConfig | Select-Object -First 1) -PSK (Get-Content $WlanConfig | Select-Object -First 1 -Skip 1)
+.\TestConnection.ps1 -Script "Start-Sleep -Seconds 60" # Wait for WLAN
 
-D:\TestConnection.ps1 -Script D:\Chocolatey.ps1 #Install Choco and Adobe Reader
+.\TestConnection.ps1 -Script .\Chocolatey.ps1 #Install Choco and Adobe Reader
 
-if (D:\CheckHP.ps1) { D:\TestConnection.ps1 -Script D:\Hpia.ps1 } #Install HPIA and update drivers
+if (.\CheckHP.ps1) { .\TestConnection.ps1 -Script .\Hpia.ps1 } #Install HPIA and update drivers
 Stop-Transcript
