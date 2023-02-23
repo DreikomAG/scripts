8f4b7ab76d158f9bed71c2536408b271dc1e76d3
Raphael Büchi <raphael.buechi@hotmail.com>
2023-02-23 08:22:56 +0100

    Delete set-env.ps1

--- set-env.ps1
+++
@@ -1,4 +0,0 @@
-# $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-# $location = Get-Location
-# $newpath = "$oldpath;$location\Azure;$location\M365;$location\Microsoft;$location\Microsoft\PermissionChanger"
-# Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
